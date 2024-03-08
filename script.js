7f402c4d8df690e237fd894df8db2113a25c01cd
Sam <162663290+SamDeemay@users.noreply.github.com>
2024-03-07 23:23:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ var i = 0;
 var hours = 1000;
 var minutes = 0;
 var seconds = 0;
-var stop = false;
-
+var decisecond = 0;
+var ms = 0;
+var stop = true;
 
 //If the sconds ar '9' it will be written as '09'
 function check1Digit(num){
@@ -15,29 +16,29 @@ function check1Digit(num){
     }
 }
 
-
 function less1Second(){
-    if (seconds == 0){
-        if(minutes == 0){
-            hours--;
-            minutes = 59;
-            seconds = 59;
+    if(ms == 0){
+        if (seconds == 0){
+            if(minutes == 0){
+                hours--;
+                minutes = 59;
+                seconds = 59;
+                ms = 99
+            }else{
+                minutes--;
+                seconds = 59;
+                ms = 99
+            }
         }else{
-            minutes--;
-            seconds = 59;
+            seconds--;
+            ms = 99
         }
     }else{
-        seconds--;
+        ms--;
     }
     document.getElementById("tempoOra").innerHTML = hours;
-    document.getElementById("tempoMinSec").innerHTML = check1Digit(minutes) + ":" + check1Digit(seconds);
+    document.getElementById("tempoMinSec").innerHTML = check1Digit(minutes) + ":" + check1Digit(seconds) + ":" + check1Digit(ms);
 }
-
-/*
-function stopTime(){
-    stop = true;
-}
-*/
 
 //Javascript web API for time
 function timeCycle(){
@@ -47,18 +48,17 @@ function timeCycle(){
         } else {
             clearInterval(intervalId);
         }
-    }, 1000);
+    }, 1);
     document.getElementById("go").disabled = true;
 }
 
 function play(){
     stop = !stop;
     if (stop == true){
-        window.alert("The clock is stopped");
+        window.alert("The clock has been stopped");
     }else{
         window.alert("The clock is running");
     }
-
     if (stop == false){
         timeCycle();
     }
@@ -81,4 +81,3 @@ function play(){
          /\\\/\
         '  `  '
 */
-
